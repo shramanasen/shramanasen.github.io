e073ad9002e803fc92fe18889dbc74690cac59a2
Shramana Sen <senshramana@gmail.com>
2025-04-22 15:48:34 +0530

    Updated

--- evaluation.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('evaluateAllButton').addEventListener('click', function() {
-    console.log("Running all analyses...");
-    
-    document.getElementById('heatmapButton').click();
-    document.getElementById('faceObjectButton').click();
-    document.getElementById('brightnessContrastButton').click();
-    
-    alert("All analyses completed (dummy implementation).");
-});
